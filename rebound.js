72fa861ff2a7ae30add00da966600792e0fece0d
jp631 <47936264+jp631@users.noreply.github.com>
2020-04-02 20:51:01 -0400

    updated the link for the sounds

--- rebound.js
+++ rebound.js
@@ -203,11 +203,11 @@ let setDifficulty = (diff)=> {
 }
 let initAudio = ()=>{
   //load audio files
-beepX = new Audio('./sounds/beepX.mp3');
-beepY = new Audio('./sounds/beepY.mp3');
-beepPaddle = new Audio('./sounds/beepPaddle.mp3');
-beepGameOver = new Audio('./sounds/beepGameOver.mp3');
-bgMusic = new Audio('./sounds/music.mp3');
+beepX = new Audio('/sounds/beepX.mp3');
+beepY = new Audio('/sounds/beepY.mp3');
+beepPaddle = new Audio('/sounds/beepPaddle.mp3');
+beepGameOver = new Audio('/sounds/beepGameOver.mp3');
+bgMusic = new Audio('/sounds/music.mp3');
 //turn off volume
 beepX.volume = 0;
 beepY.volume = 0;
